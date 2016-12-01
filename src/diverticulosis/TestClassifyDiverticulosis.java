dee8a4c83205cdac2fc780602affb41d84d19254
Anthony Fodor <anthony.fodor@gmail.com>
2016-12-01 11:35:09 -0500

    run each classifier individually...

--- src/diverticulosis/TestClassifyDiverticulosis.java
+++ src/diverticulosis/TestClassifyDiverticulosis.java
@@ -1,14 +1,13 @@
 package diverticulosis;
 
 import java.awt.Color;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.util.List;
 
 import examples.TestClassify;
+import metaMergers.BringIntoOneNameSpace;
 import parsers.NewRDPParserFileLine;
-import utils.ConfigReader;
+import projectDescriptors.AbstractProjectDescription;
 import weka.classifiers.trees.RandomForest;
 import weka.gui.visualize.ThresholdVisualizePanel;
 
@@ -17,36 +16,25 @@ public class TestClassifyDiverticulosis
 	public static void main(String[] args) throws Exception
 	{
 		int numPermutations = 50;
-
+		List<AbstractProjectDescription> projects = BringIntoOneNameSpace.getAllProjects();
+		
 		for( int x=1 ; x < NewRDPParserFileLine.TAXA_ARRAY.length; x++)
 		{
-			System.out.println(NewRDPParserFileLine.TAXA_ARRAY[x]);
-			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
-				ConfigReader.getTopeOneAtATimeDir()+ File.separator + 
-						 NewRDPParserFileLine.TAXA_ARRAY[x] +"_rocs.txt" )));
-			
-			writer.write("d1\trandom\n");
-			
-			File inArff= new File(ConfigReader.getTopeOneAtATimeDir() + File.separator + 
-					"merged" + File.separator + "pivoted_" +  
-					NewRDPParserFileLine.TAXA_ARRAY[x] + "asColumnsLogNormalPlusMetadata.arff");
-			
-			ThresholdVisualizePanel tvp = TestClassify.getVisPanel(inArff.getName());
-			
-			List<Double> firstARoc = 
-					TestClassify.plotRocUsingMultithread(
-							inArff, numPermutations, false, tvp, new RandomForest().getClass().getName(), 
-							Color.BLACK);
-			List<Double> randomROC = 
-					TestClassify.plotRocUsingMultithread(
-							inArff, numPermutations, true, tvp, new RandomForest().getClass().getName(), 
-							Color.RED);
+			String taxa = NewRDPParserFileLine.TAXA_ARRAY[x];
+			for( AbstractProjectDescription apd : projects)
+			{
+				File inArff= new File(apd.getArffMergedFileFromRDP(taxa));
 
-			
-			for(int y=0;y < firstARoc.size(); y++)
-				writer.write(firstARoc.get(y) + "\t" + randomROC.get(y) + "\n");
-			
-			writer.flush();  writer.close();
+				ThresholdVisualizePanel tvp = TestClassify.getVisPanel(
+					apd.getProjectName() + " " + taxa	);
+				TestClassify.plotRocUsingMultithread(
+						inArff, numPermutations, false, tvp, new RandomForest().getClass().getName(), 
+						Color.BLACK);
+				TestClassify.plotRocUsingMultithread(
+						inArff, numPermutations, true, tvp, new RandomForest().getClass().getName(), 
+						Color.RED);
+
+			}				
 		}
 		
 	}	
