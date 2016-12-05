19ff19dccbb82aa4e3b8e115708814dc9e2bc298
Anthony Fodor <anthony.fodor@gmail.com>
2016-12-05 14:03:34 -0500

    test one

--- src/examples/RunOneROCCurve.java
+++ src/examples/RunOneROCCurve.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.util.List;
 
 import weka.classifiers.trees.RandomForest;
-import weka.gui.visualize.ThresholdVisualizePanel;
 
 public class RunOneROCCurve
 {
@@ -17,30 +16,25 @@ public class RunOneROCCurve
 		// this file is at 
 		//https://github.com/afodor/afodor.github.io/blob/master/classes/prog2016/pivoted_genusLogNormalWithMetadata.arff
 		File inArff= new File(
-				"C:\\Users\\afodor\\git\\afodor.github.io\\classes\\prog2016\\pivoted_genusLogNormalWithMetadata.arff");
+				"/nobackup/afodor_research/arffMerged/Adenomas2012/pivoted_phylumLogNormalWithMetadata.arff");
 				
-		ThresholdVisualizePanel tvp = TestClassify.getVisPanel(inArff.getName());
+		//ThresholdVisualizePanel tvp = TestClassify.getVisPanel(inArff.getName());
 		
-		int numPermutations = 50;
-		
-		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
-				"c:\\temp\\comparisonRandomForest.txt")));
-		writer.write("notScrambled\tscrambled\n");
+		int numPermutations = 5;
 		
 		//TestClassify.plotROCForAnArff(inArff, numPermutations,random,false,tvp);	
 		//TestClassify.plotROCForAnArff(inArff, numPermutations,random,true,tvp);	
 		
 		String className = RandomForest.class.getName();
-		List<Double> notScrambled = TestClassify.plotRocUsingMultithread(inArff, numPermutations, false, tvp,
+		List<Double> notScrambled = TestClassify.plotRocUsingMultithread(inArff, numPermutations, false,null,
 						className, Color.black);
-		List<Double> scrambled =  TestClassify.plotRocUsingMultithread(inArff, numPermutations, true, tvp,
+		List<Double> scrambled =  TestClassify.plotRocUsingMultithread(inArff, numPermutations, true, null,
 						className, Color.red);
 		
 		for( int x=0; x < numPermutations; x++)
-			writer.write(notScrambled.get(x) + "\t" + scrambled.get(x) + "\n");
+			System.out.println(notScrambled.get(x) + "\t" + scrambled.get(x) + "\n");
 		
 		System.out.println("Finished in " + (System.currentTimeMillis() - startTime)/1000f + " seconds ");
 		
-		writer.flush(); writer.close();
 	}
 }
