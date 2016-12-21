74080cb119916eb7587f1a45c6a56ed182cddd3b
Anthony Fodor <anthony.fodor@gmail.com>
2016-12-21 17:08:34 -0500

    add china wgs

--- src/kraken/RunAllClassifiers.java
+++ src/kraken/RunAllClassifiers.java
@@ -13,6 +13,7 @@ import examples.TestClassify;
 import projectDescriptors.AbstractProjectDescription;
 import projectDescriptors.Adenomas2015ProjectDescriptor;
 import projectDescriptors.CRCZeller;
+import projectDescriptors.China2015_wgs;
 import projectDescriptors.IbdMetaHit;
 import projectDescriptors.CirrhosisQin;
 import utils.ConfigReader;
@@ -64,19 +65,19 @@ public class RunAllClassifiers
 	{
 		List<AbstractProjectDescription> list = new ArrayList<AbstractProjectDescription>();
 		
-		list.add(new Adenomas2015ProjectDescriptor());
+		list.add(new China2015_wgs());
 		
+		list.add(new Adenomas2015ProjectDescriptor());
 		list.add( new CRCZeller());
 		list.add( new CirrhosisQin());
 		list.add( new IbdMetaHit());
-		
 		
 		return list;
 	}
 	
 	public static void main(String[] args) throws Exception
 	{
-		int numPermutations = 50;
+		int numPermutations = 10;
 		List<AbstractProjectDescription> projects = getAllProjects();
 		
 		for( int x=TAXA_ARRAY.length-1; x >=0 ; x--)
