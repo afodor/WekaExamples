e2d29f909bbda7a094e10b539212ea72cc1b23cf
Anthony Fodor <anthony.fodor@gmail.com>
2016-12-21 10:03:39 -0500

    towards running all t-tests.

--- src/kraken/inference/RunAllTTests.java
+++ src/kraken/inference/RunAllTTests.java
@@ -3,11 +3,15 @@ package kraken.inference;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+
 import kraken.RunAllClassifiers;
 import projectDescriptors.AbstractProjectDescription;
+import utils.StatisticReturnObject;
+import utils.TTest;
 
 public class RunAllTTests
 {
@@ -20,10 +24,67 @@ public class RunAllTTests
 	
 	public static void runATTest(AbstractProjectDescription apd, String taxa) throws Exception
 	{
+		System.out.println(apd.getProjectName() + " " + taxa);
 		HashMap<String, CaseControlHolder> map = getCaseControlMap(apd, taxa);
-		System.out.println(map.size());
-		for( String s : map.keySet())
-			System.out.println(s + " " + map.get(s).caseVals.size() + " " +map.get(s).controlVals.size());
+		
+		//System.out.println(map.size());
+		//for( String s : map.keySet())
+			//System.out.println(s + " " + map.get(s).caseVals.size() + " " +map.get(s).controlVals.size());
+		
+		List<TTestResultsHolder> ttest = runTTests(map);
+		System.out.println(ttest.size());
+		
+	}
+	
+	private static List<TTestResultsHolder> runTTests(HashMap<String, CaseControlHolder> map)  
+	{
+		List<TTestResultsHolder> list = new ArrayList<TTestResultsHolder>();
+		
+		for(String s : map.keySet())
+		{
+			TTestResultsHolder t = new TTestResultsHolder(s);
+			list.add(t);
+			CaseControlHolder cch = map.get(s);
+			t.cch = cch;
+			
+			try
+			{
+				StatisticReturnObject sro = 
+						TTest.ttestFromNumberUnequalVariance(cch.controlVals,cch.caseVals );
+				
+				t.pValue = sro.getPValue();
+				t.tValue = sro.getScore();
+				
+				t.threwException = false;
+			}
+			catch(Exception ex)
+			{
+				t.threwException = true;
+			}
+		}
+		
+		Collections.sort(list);
+		return list;
+	}
+	
+	private static class TTestResultsHolder implements Comparable<TTestResultsHolder>
+	{
+		final String taxaName;
+		double pValue = 1;
+		boolean threwException = false;
+		double tValue;
+		CaseControlHolder cch;
+		
+		public TTestResultsHolder(String taxaName)
+		{
+			this.taxaName = taxaName;
+		}
+		
+		@Override
+		public int compareTo(TTestResultsHolder o)
+		{
+			return Double.compare(this.pValue, o.pValue);
+		}
 	}
 	
 	public static class CaseControlHolder 
