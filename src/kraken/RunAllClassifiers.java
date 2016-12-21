24979c13688df36b6269e0e3c2548a4aeb928870
Anthony Fodor <anthony.fodor@gmail.com>
2016-12-21 15:44:03 -0500

    bug fix

--- src/kraken/RunAllClassifiers.java
+++ src/kraken/RunAllClassifiers.java
@@ -79,7 +79,7 @@ public class RunAllClassifiers
 		int numPermutations = 50;
 		List<AbstractProjectDescription> projects = getAllProjects();
 		
-		for( int x=TAXA_ARRAY.length-1; x >=0 ; x++)
+		for( int x=TAXA_ARRAY.length-1; x >=0 ; x--)
 		{
 
 			HashMap<String, List<Double>> resultsMap = new LinkedHashMap<String,List<Double>>();
