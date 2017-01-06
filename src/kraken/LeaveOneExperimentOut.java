3db3ddb5cecf273fd15a3992306ffdcd0eabe4c3
Anthony Fodor <anthony.fodor@gmail.com>
2017-01-06 16:02:59 -0500

    fix color

--- src/kraken/LeaveOneExperimentOut.java
+++ src/kraken/LeaveOneExperimentOut.java
@@ -98,7 +98,7 @@ public class LeaveOneExperimentOut
 				results.addAll(getAoc(
 						apd, 1, false, tvp, classifierName, Color.red, taxa));
 				results.addAll(getAoc(
-						apd, 50, true, tvp, classifierName, Color.red, taxa));
+						apd, 50, true, tvp, classifierName, Color.BLACK, taxa));
 				writeResults(resultsMap, taxa, classifierName);
 			
 			}
