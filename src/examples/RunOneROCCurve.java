213d3808b9706abdac18762b1acc5d6613eba7e7
afodor <afodor@DESKTOP-T44GFRG>
2021-09-28 15:34:49 -0400

    updated multi-threading example

--- src/examples/RunOneROCCurve.java
+++ src/examples/RunOneROCCurve.java
@@ -1,21 +1,34 @@
 package examples;
 
+import java.awt.Color;
 import java.io.File;
 import java.util.Random;
 
+import weka.classifiers.trees.RandomForest;
 import weka.gui.visualize.ThresholdVisualizePanel;
 
 public class RunOneROCCurve
 {
 	public static void main(String[] args) throws Exception
 	{
+		long startTime = System.currentTimeMillis();
 		Random random = new Random();
-		// this file is at https://github.com/afodor/afodor.github.io/blob/master/classes/prog2016/pivoted_genusLogNormalWithMetadata.arff
+		// this file is at 
+		//https://github.com/afodor/afodor.github.io/blob/master/classes/prog2016/pivoted_genusLogNormalWithMetadata.arff
 		File inArff= new File(
 				"C:\\temp\\pivoted_genusLogNormalWithMetadata.arff");
 				
 		ThresholdVisualizePanel tvp = TestClassify.getVisPanel(inArff.getName());
-				
-		TestClassify.plotROCForAnArff(inArff, 1,random,false,tvp);	
+		
+		int numPermutations = 20;
+		
+		TestClassify.plotRocUsingMultithread(inArff, numPermutations, false, tvp, RandomForest.class.getName(), Color.BLACK);
+
+		TestClassify.plotRocUsingMultithread(inArff, numPermutations, true, tvp, RandomForest.class.getName(), Color.RED);
+		
+		//TestClassify.plotROCForAnArff(inArff, numPermutations,random,false,tvp);	
+		//TestClassify.plotROCForAnArff(inArff, numPermutations,random,true,tvp);	
+		
+		System.out.println("Finished in " + (System.currentTimeMillis() - startTime)/1000f + " seconds ");
 	}
 }
