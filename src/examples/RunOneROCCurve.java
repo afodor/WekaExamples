0b1abd8d7c55af19922a9ef778e454daa7931b2d
afodor <afodor@DESKTOP-T44GFRG>
2021-09-28 15:17:00 -0400

    updated jar files

--- src/examples/RunOneROCCurve.java
+++ src/examples/RunOneROCCurve.java
@@ -1,40 +1,21 @@
 package examples;
 
-import java.awt.Color;
 import java.io.File;
-import java.util.List;
+import java.util.Random;
 
-import weka.classifiers.trees.RandomForest;
+import weka.gui.visualize.ThresholdVisualizePanel;
 
 public class RunOneROCCurve
 {
-	// run on the cluster like this:
-	//  java -cp .:/users/afodor/gitInstall/WekaExamples/weka.jar examples.RunOneROCCurve
 	public static void main(String[] args) throws Exception
 	{
-		long startTime = System.currentTimeMillis();
-		// this file is at 
-		//https://github.com/afodor/afodor.github.io/blob/master/classes/prog2016/pivoted_genusLogNormalWithMetadata.arff
+		Random random = new Random();
+		// this file is at https://github.com/afodor/afodor.github.io/blob/master/classes/prog2016/pivoted_genusLogNormalWithMetadata.arff
 		File inArff= new File(
-				"/nobackup/afodor_research/arffMerged/Adenomas2012/pivoted_phylumLogNormalWithMetadata.arff");
+				"C:\\Users\\corei7\\git\\afodor.github.io\\classes\\prog2016\\pivoted_genusLogNormalWithMetadata.arff");
 				
-		//ThresholdVisualizePanel tvp = TestClassify.getVisPanel(inArff.getName());
-		
-		int numPermutations = 5;
-		
-		//TestClassify.plotROCForAnArff(inArff, numPermutations,random,false,tvp);	
-		//TestClassify.plotROCForAnArff(inArff, numPermutations,random,true,tvp);	
-		
-		String className = RandomForest.class.getName();
-		List<Double> notScrambled = TestClassify.plotRocUsingMultithread(inArff, numPermutations, false,null,
-						className, Color.black);
-		List<Double> scrambled =  TestClassify.plotRocUsingMultithread(inArff, numPermutations, true, null,
-						className, Color.red);
-		
-		for( int x=0; x < numPermutations; x++)
-			System.out.println(notScrambled.get(x) + "\t" + scrambled.get(x) + "\n");
-		
-		System.out.println("Finished in " + (System.currentTimeMillis() - startTime)/1000f + " seconds ");
-		
+		ThresholdVisualizePanel tvp = TestClassify.getVisPanel(inArff.getName());
+				
+		TestClassify.plotROCForAnArff(inArff, 1,random,false,tvp);	
 	}
 }
